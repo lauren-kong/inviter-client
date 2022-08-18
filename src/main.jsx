f2add60cfcd0dffe0fae947561d45fa1afb068e0
lauren <Minji@lauren.local>
2022-08-19 08:45:12 +1200

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import reducers from './reducers'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const store = configureStore({ reducer: reducers })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 )
